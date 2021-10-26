0d9bf2787d42c6d9f65e5adbe084e21acee8ce9f
Kyle Evangelisto <kyleevangelisto@gmail.com>
2021-10-26 19:48:14 -0400

    remove duplicated js from app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,1 @@
 require('./bootstrap');
-
-$(document).ready(function (e) {
-  $('#image').change(function(){     
-    let reader = new FileReader();
-    reader.onload = (e) => { 
-      $('#preview-image-before-upload').attr('src', e.target.result); 
-    }
-
-    reader.readAsDataURL(this.files[0]); 
-  });
-});
