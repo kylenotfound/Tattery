5b18ab64643b20b0bd7e79eb4b92bdd5a813846a
gabrieldontsnap <gabriellemarie@knights.ucf.edu>
2021-10-27 19:41:13 -0700

    Rearranged content for profile & made it so that Edit Porfile button only appears on own account

--- resources/views/dash.blade.php
+++ resources/views/dash.blade.php
@@ -31,75 +31,73 @@
 
   <!-- User's profile page every user can see -->
   <div class="container">
-    <div class="row align-items-center justify-content-center">
-      <div class="col-md-4 col-sm-auto">
-        <img src="{{ $avatar }}" class="rounded-circle" width="200" height="200">
+    <div class="row d-flex align-items-center">
+      <div class="col col-sm-6 d-flex justify-content-center">
+        <img src="{{ $avatar }}" class="rounded-circle mb-4 mb-sm-0" width="200" height="200">
       </div>
 
-      <div class="col-md-8 col-sm-auto">
-        <p class="h3">{{$user->getDisplayName()}}</p>
+      <div class="col col-sm-6  text-left">
+        <p class="h5 my-0">{{$user->getDisplayName()}}</p>
         <br>
-        <span>{{$user->getName()}}</span>
-        <br>
-        <span>About {{$user->getDisplayName()}}</span>
-        <br>
-        <span>Pronouns: {{$user->getPronouns()}}</span>
+        <span><b>{{$user->getName()}}</b>
+          <span style="font-size: 16px;" class="ml-1"><i>Age: {{$user->getAge()}} </i>| <i>Pronouns: {{$user->getPronouns()}} </i>| <i>Virgin Status: {{$user->getVirginStatus()}}</i></span>
+        </span>
         <br>
         <span>{{$user->getBio()}}</span>
-        <br>
-        <span>{{$user->getAge()}}</span>
-        <br>
-        <span>Virgin Status: {{$user->getVirginStatus()}}</span>
       </div>
     </div>
   </div>
 
   <br>
 
-  <div class="btn-group btn-group-lg">
-    <button type="button" data-section="section1" class="btn btn-primary segmentedButton ">Posts</button>
-    <button type="button" data-section="section2" class="btn btn-primary segmentedButton">Update Profile</button>
-  </div>
+  <div class="container">
+    <div class="btn-group btn-group-sm d-flex flex-row justify-content-evenly">
+      <button type="button" data-section="section1" class="btn btn-primary segmentedButton ">Posts</button>
+      @if(Auth::user()->id == $user->id)
+      <button type="button" data-section="section2" class="btn btn-primary segmentedButton">Edit Profile</button>
+      @endif
+    </div>
 
-  <div class="content-section" id="section1">
-    <p>Posts will eventually go here</p>
-  </div>
+    <div class="content-section" id="section1">
+      <p>Posts will eventually go here</p>
+    </div>
 
-  <div class="content-section" id="section2">
-    <!--If the user logged in is the same user that owns the profile, display profile customization features -->
+    <div class="content-section" id="section2">
+      <!--If the user logged in is the same user that owns the profile, display profile customization features -->
 
-    @if(Auth::user()->id == $user->id)
-      <div>
-        <span>Update Username</span>
-        <form enctype="multipart/form-data" action="{{route('dash.update_profile', ['id' => $user->getDisplayName()])}}" method="POST">
-          @csrf
-          <label>Change Photo</label>
-          <input type="file" name="avatar"></input>
-          <br>
-          <label>Change Name</label>
-          <input type="text" name="name" value="{{$user->getName()}}"></input>
-          <br>
-          <label>Change Username</label>
-          <input type="text" name="new_display_name" value="{{$user->getDisplayName()}}"></input>
-          <span>Change Pronouns</span>
-          <input type="text" name="pronouns" value="{{$user->getPronouns()}}"></input>
-          <span>Change Bio</span>
-          <input type="text" name="bio" value="{{$user->getBio()}}"></input>
-          <br>
-          <span>Change Age</span>
-          <input type="text" name="age" value="{{$user->getAge()}}"></input>
-          <br>
-          <label>Change Virgin Status</label>
-          <select name="virgin_status">
-            <option value="Virgin">Virgin</option>
-            <option value="Non virgin">Not a Virgin</option>
-            <option vlaue="N/A">N/A</option>
-          </select>
-          <br>
-          <input type="submit"></input>
-        </form>
-      </div>
-    @endif
+      @if(Auth::user()->id == $user->id)
+        <div>
+          <span>Update Username</span>
+          <form enctype="multipart/form-data" action="{{route('dash.update_profile', ['id' => $user->getDisplayName()])}}" method="POST">
+            @csrf
+            <label>Change Photo</label>
+            <input type="file" name="avatar"></input>
+            <br>
+            <label>Change Name</label>
+            <input type="text" name="name" value="{{$user->getName()}}"></input>
+            <br>
+            <label>Change Username</label>
+            <input type="text" name="new_display_name" value="{{$user->getDisplayName()}}"></input>
+            <span>Change Pronouns</span>
+            <input type="text" name="pronouns" value="{{$user->getPronouns()}}"></input>
+            <span>Change Bio</span>
+            <input type="text" name="bio" value="{{$user->getBio()}}"></input>
+            <br>
+            <span>Change Age</span>
+            <input type="text" name="age" value="{{$user->getAge()}}"></input>
+            <br>
+            <label>Change Virgin Status</label>
+            <select name="virgin_status">
+              <option value="Virgin">Virgin</option>
+              <option value="Non virgin">Not a Virgin</option>
+              <option vlaue="N/A">N/A</option>
+            </select>
+            <br>
+            <input type="submit"></input>
+          </form>
+        </div>
+      @endif
+    </div>
   </div>
 
   <script type="text/javascript">
